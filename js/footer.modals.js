f5ab2bec182be421d10a9230ae9389fe7d024a53
Gerardo Barud <gbarud@unsj.edu.ar>
2022-03-23 21:06:58 -0300

    first commit

---
+++ js/footer.modals.js
@@ -0,0 +1,6 @@
+(function($) {
+  	"use strict"; // Start of use strict
+	$("#termsModal").load("footer-terms-modal.html"); 
+	$("#aboutModal").load("footer-about-modal.html"); 
+
+})(jQuery); // End of use strict
---
+++ js/main.navigation.js
@@ -0,0 +1,5 @@
+(function($) {
+  	"use strict"; // Start of use strict
+	$("#mainNavigation").load("main-navigation.html"); 
+
+})(jQuery); // End of use strict
